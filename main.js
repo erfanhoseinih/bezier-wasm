14938a9eb10eddf242ea941e001ce78c4aafc491
erfanhoseini <105305840+erfannhoseini@users.noreply.github.com>
2024-04-05 11:42:59 +0330

    include bezierwasm.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function main() {
 
 
 
-async function wasmtest() {
+function wasmtest() {
 
 
     let curvelinesNum = document.getElementById("inputnum").value;
@@ -28,46 +28,6 @@ async function wasmtest() {
     let result0 = [];
 
 
-    var exports;
-    var memory;
-
-    memory = new WebAssembly.Memory({
-        initial: 256,
-        maximum: 512
-    })
-
-    await WebAssembly.instantiateStreaming(fetch("./wasmBezier/bezier.wasm"), {
-        js: {
-            mem: memory
-        },
-        env: {
-            emscripten_resize_heap: memory.grow
-        }
-    }).then(result => {
-        exports = result.instance.exports;
-        memory = result.instance.exports.memory;
-    })
-
-
-    function curveBezier(arr, detail) {
-        var input_array = new Float32Array(arr);
-        var len = input_array.length;
-        var bytes_per_element = input_array.BYTES_PER_ELEMENT;
-
-        var currntArr = exports.wasmmalloc(len * bytes_per_element);
-        var input_ptr = exports.wasmmalloc(len * bytes_per_element);
-        var output_ptr = exports.wasmmalloc(len * bytes_per_element);
-
-        let summands = new Float32Array(memory.buffer, input_ptr);
-        for (let i = 0; i < len; i++) { summands[i] = input_array[i]; }
-
-
-        exports.curveBezier(input_ptr, output_ptr, currntArr, len, detail);
-
-        var output_array = new Float32Array(memory.buffer, output_ptr, detail * 2);
-
-        return output_array;
-    }
 
 
     for (let km = 0; km < curvelinesNum; km++) {
